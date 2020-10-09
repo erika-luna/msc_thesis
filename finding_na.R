31f80a221ed4c3a287a7d220c9e573408fd4cae3
Erika Luna <erikaluna@Taro.hitronhub.home>
2020-10-08 23:14:51 -0700

    number of observations for each crop at every state

--- finding_na.R
+++ finding_na.R
@@ -6,9 +6,11 @@ library(dplyr)
 library(ggplot2)
 
 
-AGS <- SIAP %>% 
+#AGS <- SIAP %>% 
+BC <- SIAP %>% 
   #filter(state == "aguascalientes", crop == "aceituna") %>% 
-  filter(state == "aguascalientes") %>% 
+  #filter(state == "aguascalientes", type == "food") %>% 
+  filter(state == "baja california", type == "food") %>% 
   #select(crop:year)# %>% 
   group_by(crop, year) %>% 
  summarise(sum_prod = sum(production), .groups = 'drop')# %>% 
@@ -21,7 +23,8 @@ period <- tibble(c(1980:2016))
 colnames(period) <- c("year")
 
 
-amigo <- left_join(period, AGS)
+#amigo <- left_join(period, AGS)
+amigo <- left_join(period, BC)
 #tmp <- left_join(period, AGS)
 #tmp <- union_all(period, AGS)
 #tmp <- cbind(AGS, period)
@@ -32,15 +35,64 @@ amiga <- amigo %>%
   summarise(obs = sum(!is.na(sum_prod)))  # of non-NA’s )
  # DT::datatable()
   
-AGS_obs <- amiga %>% 
+#AGS_obs <- amiga %>% 
+BC_obs <- amiga %>% 
   ggplot(aes(crop, obs)) +
   geom_bar(stat = "identity") +
-AGS_obs
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_y_continuous(name = "number of observations") 
+#AGS_obs
+BC_obs
+
+#### state vs. number of obs for one crop ####
+
+aceituna <- SIAP %>% 
+  #filter(state == "aguascalientes", crop == "aceituna") %>% 
+  #filter(state == "aguascalientes", type == "food") %>% 
+  filter(crop == "aceituna") %>% 
+  #select(crop:year)# %>% 
+  group_by(year, state) %>% 
+  summarise(sum_prod = sum(production), .groups = 'drop')# %>% 
+
+aceituna_full <- na.omit(aceituna)
+tmp <- aceituna_full %>% 
+  group_by(state) %>% 
+  summarise(obs = sum(!is.na(sum_prod)))
 
 
-tmp <- tmp %>% 
-  group_by(crop, year)
-  
-str(AGS$crop)    
+aceituna_obs <- aceituna %>% 
+  group_by(state) %>% 
+  summarise(obs = sum(!is.na(sum_prod)))
+
+aceituna_state_obs <- aceituna_obs %>% 
+  ggplot(aes(state, obs)) +
+  geom_bar(stat = "identity")
+aceituna_state_obs
+
+#### 
+
+one_crop <- SIAP %>% 
+  #filter(state == "aguascalientes", crop == "aceituna") %>% 
+  #filter(state == "aguascalientes", type == "food") %>% 
+  filter(crop == "jitomate") %>% 
+  #select(crop:year)# %>% 
+  group_by(year, state) %>% 
+  summarise(sum_prod = sum(production), .groups = 'drop')# %>% 
+
+crop_full <- one_crop %>% 
+  group_by(state) %>% 
+  summarise(obs = sum(!is.na(sum_prod)))
+
+plot_obs <- crop_full %>% 
+  ggplot(aes(state, obs)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_y_continuous(name = "number of observations") 
+plot_obs
+####
+
+
+
+
 
 
